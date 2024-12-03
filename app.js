bbd925ee2d42cf34cbdf5ae4c6fbf8289a3c6fa3
Aaira Khan <aairasalmankhan2008@gmail.com>
2024-12-03 17:24:05 +0500

    task done

---
+++ app.js
@@ -0,0 +1,8 @@
+var age = "I'm 15 years old";
+alert (age)
+
+var times = "You have visited this webpage 1st time";
+alert (times)
+
+
+document.write ("Welcome to javascript 3rd assignment.........My Birth Year is 2008")
